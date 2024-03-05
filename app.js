f764b277d8c29ec4510c3ff9128c3f7969d539c2
rbock9 <rbock9@gmail.com>
2024-03-05 12:43:40 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const flash = require('connect-flash')
+const markdown = require('marked')
+const app = express()
+const sanitizeHTML = require('sanitize-html')
+
+let sessionOptions = session({
+    secret: "JavaScript is so cool",
+    store: MongoStore.create({client: require('./db')}),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
+})
+
+app.use(sessionOptions)
+app.use(flash())
+
+app.use(function(req, res, next) {
+    // make markdown function available from within ejs templates
+    res.locals.filterUserHTML = function(content) {
+        return sanitizeHTML(markdown.parse(content), {allowedTags: ['p', 'br', 'ul', 'ol', 'li', 'strong', 'bold', 'i', 'em', 'u', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'], allowedAttributes: {}})
+    }
+
+    // make all eror and success flash messages available from all templates
+    res.locals.errors = req.flash("errors")
+    res.locals.success = req.flash("success")
+
+    // make current user id available on the req object
+    if (req.session.user) {req.visitorId = req.session.user._id} else {req.visitorId = 0}
+
+    // make user session data available from within view templates
+    res.locals.user = req.session.user
+    next()
+})
+
+const router = require('./router.js')
+
+app.use(express.urlencoded({extended: false})) 
+app.use(express.json())  
+
+app.use(express.static('public'))
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
+app.use('/', router)
+
+module.exports = app
